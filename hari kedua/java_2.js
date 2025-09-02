08e2fb3c5242863ce47af5d2250a9856eef184f6
ayash301 <anuwayyif@gmail.com>
2025-09-02 13:50:55 +0700

    udah kelar

---
+++ hari kedua/java_2.js
@@ -0,0 +1,19 @@
+
+let umur = 20;           
+const nama = "Ayash";     
+var status = true;       
+
+
+let a = 10;
+let b = 5;
+let hasilAritmatika = a + b;   
+
+
+let hasilPerbandingan = a > b; 
+
+
+console.log("Umur:", umur);
+console.log("Nama:", nama);
+console.log("Status:", status);
+console.log("Hasil Aritmatika (10 + 5):", hasilAritmatika);
+console.log("Apakah 10 lebih besar dari 5?", hasilPerbandingan);

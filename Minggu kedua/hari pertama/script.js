9fc369476d797b995b6e246d2866dac6f16091a5
ayash301 <anuwayyif@gmail.com>
2025-09-09 14:43:22 +0700

    ya

---
+++ Minggu kedua/hari pertama/script.js
@@ -0,0 +1,158 @@
+//llatihan mandiri
+//soal 1
+let temanSekelas = ["Ryan", "Besi", "Yeyen", "Yamin", "gopal"];
+console.table(temanSekelas);
+
+//soal 2
+let temanSekelas2 = ["Ryan", "Besi", "Yeyen", "Yamin", "gopal"];
+temanSekelas2.push("Rudi")
+temanSekelas2.unshift("Roni")
+console.table(temanSekelas2);
+
+//soal 3
+let temanSekelas3 = ["Ryan", "Besi", "Yeyen", "Yamin", "gopal"];
+temanSekelas3.pop()
+temanSekelas3.shift()
+console.table(temanSekelas3);
+
+//soal 4
+let temanSekelas4 = ["Ryan", "Besi", "Yeyen", "Yamin", "gopal"];
+temanSekelas4.splice(2,0,"Roni")
+console.table(temanSekelas4);
+
+//soal 5
+let temanSekelas5 = ["Ryan", "Besi", "Yeyen", "Yamin", "gopal"];
+console.log(temanSekelas5.length);
+
+
+
+//soal1
+let buah = ["apel", "mangga", "pisang"];
+buah.push("Kelapa")
+buah.unshift("Durian")
+console.table(buah);
+
+//soal2
+let belanja = ["Beras", "Telur", "Susu", "Gula"];
+belanja.push("Minyak Goreng")
+console.table(belanja);
+
+//soal3
+let hewan = ["Kucing", "Anjing", "Kelinci"];
+hewan.unshift("Ikan")
+console.table(hewan);
+
+//soal4
+let film = ["LOTR", "Avenger", "Spiderman"];
+film.push("Thor")
+console.table(film);
+
+//soal5
+let angka = [1, 2, 3, 4, 5];
+angka.unshift(0)
+console.table(angka);
+
+//soal6
+let mainan = ["Mobil", "Motor", "Bola"];
+mainan.pop()
+console.table(mainan);
+
+//soal7
+let baju = ["Kaos", "Kemeja", "Jaket"];
+baju.shift()
+console.table(baju);
+
+//soal8
+let makanan = ["Nasi Goreng", "Mie Ayam", "Sate"];
+makanan.pop()
+console.table(makanan);
+
+//soal9
+let tugasSekolah = ["Matematika", "Bahasa Indonesia", "IPA"];
+tugasSekolah.shift()
+console.table(tugasSekolah);
+
+//soal10
+let antrian = ["Andi", "Budi", "Caca"];
+antrian.shift()
+console.table(antrian);
+
+//soal11
+let buah2 = ["Jeruk", "Melon", "Semangka", "Mangga", "Apel", "Pisang", "Durian"];
+console.log(buah2.length);
+
+//soal12
+let teman = ["Rina", "Sita", "Tina", "Lina", "Mina","Vina"];
+console.log(teman.length);
+
+//soal13
+let angka2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+console.log(angka2.length);
+
+//soal14
+let daftarBelanja = ["Minyak Goreng", "Beras", "Gula", "Telur", "Susu", "Tepung", "Kopi", "Teh"];
+console.log(daftarBelanja.length);
+
+//soal15
+let hewan2 = ["Sapi", "Kambing", "Ayam", "Bebek", "Ikan"];
+console.log(hewan2.length);
+
+//soal16
+let warna = ["merah", "kuning", "hijau"]
+warna.splice(2,0 ,"biru")
+console.table(warna);   
+
+//soal17
+let teman2 = ["Andi", "Budi", "Caca", "Deni"]
+teman2.splice(3,1)
+console.table(teman2);
+
+//soal18
+let angka3 = [1,2,3,4,5]
+angka3.splice(2,1,30 )
+console.table(angka3);
+
+//soal19
+let makanan2 = ["Nasi Goreng", "Mie Ayam", "Sate", "Bakso"]
+makanan2.splice(2,0,"Gado-gado")
+console.table(makanan2);
+
+//soal20
+let benda = ["Buku", "Pensil", "Penghapus",]
+benda.splice(1,1,"Spidol")
+console.table(benda);
+
+//soal21
+let daftarBelanja2 = ["Minyak Goreng", "Beras", "Gula", "Telur", "Susu", "Tepung", "Kopi", "Teh"];
+daftarBelanja2.push("Coklat")
+daftarBelanja2.shift()
+console.table(daftarBelanja2);
+
+//soal22
+let hewanpeliharaan = ["Kucing", "Anjing", "Kelinci"];
+hewanpeliharaan.push("Babi")
+hewanpeliharaan.shift()
+hewanpeliharaan.splice(1,0,"Ikan")
+console.table(hewanpeliharaan);
+
+//soal23
+let angka4 = [1,2,3,4,5]
+angka4.push(6)
+angka4.shift()
+console.table(angka4);
+ 
+//soal24
+let teman3 = ["Rina", "Sita", "Tina", "Lina", "Mina","Vina"];
+teman3.unshift("Nina")
+teman3.pop()
+teman3.splice(2,1,"Dina")
+console.table(teman3);
+
+//soal25
+let makanan3 = ["Nasi Goreng", "Mie Ayam", "Sate"];
+makanan3.push("Bakso")
+makanan3.shift()    
+makanan3.splice(1,0,"Gado-gado")
+console.table(makanan3);
+
+
